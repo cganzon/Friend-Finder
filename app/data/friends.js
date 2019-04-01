e830b64733eb1397cd3b6b63d28c075c3f4d89aa
cganzon <crganzon@gmail.com>
2019-03-31 22:53:38 -0700

    friend finder logic and modal

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,14 +1,24 @@
 // List of all the friends currently in the Friend Finder app
 const friends = [
     {
-        name: "Carl",
-        photo: "https://vignette.wikia.nocookie.net/jimmyneutron/images/7/71/Carl.png/revision/latest?cb=20180809091358",
-        scored: [3, 2, 5]
+        name: "Eric Cartman",
+        photo: "https://vignette.wikia.nocookie.net/southpark/images/c/c4/Eric-cartman.png/revision/latest/scale-to-width-down/310?cb=20180718082504",
+        scores: [4, 2, 1]
     },
     {
-        name: "Stan",
-        photo: "https://vignette.wikia.nocookie.net/cartoon-network-city-fanon/images/c/c6/Stan-marsh-0.png/revision/latest?cb=20180129005848",
-        scored: [4, 2, 2]
+        name: "Stan Marsh",
+        photo: "https://vignette.wikia.nocookie.net/southpark/images/c/c6/Stan-marsh-0.png/revision/latest/scale-to-width-down/310?cb=20170721155858",
+        scores: [4, 5, 2]
+    },
+    {
+        name: "Bebe Stevens",
+        photo: "https://vignette.wikia.nocookie.net/southpark/images/2/2b/2-436.png/revision/latest/scale-to-width-down/310?cb=20160402112834",
+        scores: [2, 5, 3]
+    },
+    {
+        name: "Wendy Tesaburger",
+        photo: "https://vignette.wikia.nocookie.net/southpark/images/9/9e/Wendyy.png/revision/latest/scale-to-width-down/310?cb=20171014061739",
+        scores: [3, 3, 3]
     },
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,12 +2,39 @@
 const friendsList = require("../data/friends");
 
 module.exports = (app) => {
+    // Routes
     app.get("/api/friends", (req, res) => {
         res.json(friendsList);
     });
 
-    // Adding to friends list API after user takes the survey
+    // Adding to friends API after user takes the survey
     app.post("/api/friends", (req, res) => {
-            friendsList.push(req.body);
+
+        console.log(req.body);
+        for (let i = 0; i < req.body.scores.length; i++) {
+            // Converting scores to integers
+            req.body.scores[i] = parseInt(req.body.scores[i]);
+        };
+        // Initial best friend data to compare to
+        let bestFriend = friendsList[0];
+        let minDiff = 50;
+        // Best friend search logic
+        for (let i = 0; i < friendsList.length; i++) {
+            let totalDiff = 0;
+            for (let j = 0; j < friendsList[i].scores.length; j++) {
+                let scoreDiff = Math.abs(req.body.scores[j] - friendsList[i].scores[j]);
+                totalDiff += scoreDiff;
+                console.log(totalDiff);
+            };
+
+            if (totalDiff < minDiff) {
+                minDiff = totalDiff;
+                bestFriend = friendsList[i];
+            };
+        };
+        // Adding user to friends array after search is done
+        friendsList.push(req.body);
+        // Sending back response of the user's best friend match
+        res.json(bestFriend);
     });
 };
